cc4ac2cb9709d15f246decabee524a562210ce89
Y.MOLIKA <163737690+molika12@users.noreply.github.com>
2025-09-14 22:37:05 +0530

    Update shopify.js

--- routes/shopify.js
+++ routes/shopify.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { getCustomers, getOrders, getProducts } = require('../controllers/shopifyController');
 
-// Tenant-aware APIs
 router.get('/customers', getCustomers);
 router.get('/orders', getOrders);
 router.get('/products', getProducts);
 
 module.exports = router;
+
