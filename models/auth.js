655c16f0568a389e7c09c7c3a86a22ecdc4d8cd1
Y.MOLIKA <163737690+molika12@users.noreply.github.com>
2025-09-14 22:36:06 +0530

    Update auth.js

--- models/auth.js
+++ models/auth.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { signupTenant, loginTenant } = require('../controllers/authController');
 
-// POST /signup → calls your signupTenant function
 router.post('/signup', signupTenant);
-
-// POST /login → calls your loginTenant function
 router.post('/login', loginTenant);
 
 module.exports = router;
+
