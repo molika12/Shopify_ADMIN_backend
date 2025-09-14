4dea8a93717747ba9f0ab37e8b80afa096c23f0f
Y.MOLIKA <163737690+molika12@users.noreply.github.com>
2025-09-14 22:38:30 +0530

    Update verifyWebhook.js

--- utils/verifyWebhook.js
+++ utils/verifyWebhook.js
@@ -1,10 +1,9 @@
-// utils/verifyWebhook.js
 const crypto = require('crypto');
 
 function verifyShopifyWebhook(req, res, next) {
   const hmacHeader = req.get('X-Shopify-Hmac-Sha256');
-  const body = req.rawBody; // we need raw body, see server.js setup below
-  const secret = process.env.SHOPIFY_API_SECRET; // put your Shopify API secret in .env
+  const body = req.rawBody; 
+  const secret = process.env.SHOPIFY_API_SECRET; 
 
   const hash = crypto
     .createHmac('sha256', secret)
@@ -12,10 +11,11 @@ function verifyShopifyWebhook(req, res, next) {
     .digest('base64');
 
   if (hash === hmacHeader) {
-    next(); // verified
+    next(); 
   } else {
     res.status(401).send('Webhook verification failed');
   }
 }
 
 module.exports = verifyShopifyWebhook;
+
